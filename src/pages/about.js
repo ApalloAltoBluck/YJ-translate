535a2f1be02d29a99a1c2de4d2bd684c46166fd0
TomasCarlson <34401946+TomasCarlson@users.noreply.github.com>
2021-04-01 12:46:58 -0400

    About fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,25 +21,29 @@ function AboutPage(props) {
       </div>
       <div class="col-sm text-justify d-flex flex-column" style={{  height: '100vh'}}>
         <div class="my-auto">
-          <h1 id="oliver-tag" class="my-auto text-center  p-2 mx-auto" onMouseEnter={e => {
-            document.getElementById("face-back").src=tomasOliver; document.body.style.backgroundColor = '#FE5D26';
-            document.body.style.color = '#fff';
-            document.getElementById("tomas-tag").style.color = '#fff';
-            document.getElementById("tomas-tag").style.backgroundColor = '';
-            document.getElementById("oliver-tag").style.color = '#FE5D26';
-            document.getElementById("oliver-tag").style.backgroundColor = '#fff';}}>
-            Oliver Buckley
-          </h1>
+        <a href="https://blucksy.com/" className="about-link">
+            <h1 id="oliver-tag" class="my-auto text-center  p-2 mx-auto" onMouseEnter={e => {
+              document.getElementById("face-back").src=tomasOliver; document.body.style.backgroundColor = '#FE5D26';
+              document.body.style.color = '#fff';
+              document.getElementById("tomas-tag").style.color = '#fff';
+              document.getElementById("tomas-tag").style.backgroundColor = '';
+              document.getElementById("oliver-tag").style.color = '#FE5D26';
+              document.getElementById("oliver-tag").style.backgroundColor = '#fff';}}>
+              Oliver Buckley
+            </h1>
+          </a>
           <img id="face-back" class="my-auto mx-auto p-2" src={tomas}></img>
-          <h1 id="tomas-tag" class="my-auto text-center p-2 mx-auto" onMouseEnter={e => {
+          <a href="https://tomascarlson.com/" className="about-link">
+            <h1 id="tomas-tag" class="my-auto text-center p-2 mx-auto" onMouseEnter={e => {
             document.getElementById("face-back").src=oliverTomas; document.body.style.backgroundColor = '#DE4040';
             document.body.style.color = '#fff';
             document.getElementById("oliver-tag").style.color = '#fff';
             document.getElementById("oliver-tag").style.backgroundColor = '';
             document.getElementById("tomas-tag").style.color = '#DE4040';
             document.getElementById("tomas-tag").style.backgroundColor = '#fff';}}>
-            Tomás Carlson
+              Tomás Carlson
           </h1>
+          </a>
         </div>
       </div>
     </div>
