ad7937192b5f397ff83e6f3f4972f1a8fadcafbf
TomasCarlson <34401946+TomasCarlson@users.noreply.github.com>
2021-04-01 16:50:26 -0400

    fixed on small screens

--- src/pages/clients.js
+++ src/pages/clients.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Row, Col, Alert, Image, Container} from 'react-bootstrap/'
+import { Row, Col, Alert, Image, Container } from 'react-bootstrap/'
 import Header from "./templates/header"
 import who from "../images/WHO.png"
 import varfaj from "../images/varfaj.png"
@@ -14,35 +14,38 @@ import Logoicon from "../components/logo-icon"
 function ClientsPage(props) {
 
   return (<>
-  <Row className="sidebar">
+    <Row className="sidebar">
+      <Header></Header>
+      <Col dismissible variant="danger">
 
-  <Header></Header>
-  <Row>
-  <Container> 
-     <Row>
-    <Col className="icon-logo">
-    <Logoicon link={who} ></Logoicon>
-    </Col>
-    <Col className="icon-logo">
-    <Logoicon link={varfaj}></Logoicon>
-    </Col>
-    <Col className="icon-logo">
-    <Logoicon link={he}></Logoicon>
-    </Col>
+        <Row>
+          <Container>
+            <Row>
+              <Col className="icon-logo">
+                <Logoicon link={who} ></Logoicon>
+              </Col>
+              <Col className="icon-logo">
+                <Logoicon link={varfaj}></Logoicon>
+              </Col>
+              <Col className="icon-logo">
+                <Logoicon link={he}></Logoicon>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="icon-logo">
+                <Logoicon link={rough}></Logoicon>
+              </Col>
+              <Col className="icon-logo">
+                <Logoicon link={wrbb}></Logoicon>
+              </Col>
+              <Col className="icon-logo">
+                <Logoicon link={maker}></Logoicon>
+              </Col>
+            </Row>
+          </Container>
+        </Row>
+      </Col>
     </Row>
-    <Row>
-    <Col className="icon-logo">
-    <Logoicon link={rough}></Logoicon>
-    </Col>
-    <Col className="icon-logo">
-    <Logoicon link={wrbb}></Logoicon>
-    </Col>
-    <Col className="icon-logo">
-    <Logoicon link={maker}></Logoicon>
-    </Col>
-    </Row>
-  </Container></Row>
-  </Row>
   </>)
 }
 
